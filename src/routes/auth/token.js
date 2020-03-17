77473bfc85744027d9dc00048a03c814e581c8df
tvOdyssey <43388217+tvOdyssey@users.noreply.github.com>
2020-03-17 19:25:13 -0400

    Initial Commits of the me.js and token.js files.

---
+++ src/routes/auth/token.js
@@ -0,0 +1,6 @@
+/*
+verify sent username and password,
+respond with encrypted token that is saved in the db in accordance with that account
+
+(future security = limit responses with a username, and from specific hosts)
+*/
---
+++ src/routes/user/me.js
@@ -0,0 +1,11 @@
+/*
+check if token exists in the db,
+if so, respond with the users data (minus pass)
+
+username,
+email,
+profile,
+location,
+
+creation_date
+*/
