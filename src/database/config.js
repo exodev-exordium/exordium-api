597d9bee540ee7396e2eb09ad45b40173e480686
tvOdyssey <43388217+tvOdyssey@users.noreply.github.com>
2020-04-04 03:39:34 -0400

    Remove the old mysql db setup

--- src/database/config.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config()
-
-module.exports = {
-  hostname: process.env.DB_HOSTNAME || "localhost",
-  username: process.env.DB_USERNAME || "root",
-  password: process.env.DB_PASSWORD || "",
-  database: process.env.DB_DATABASE || "exordium_api"
-}
