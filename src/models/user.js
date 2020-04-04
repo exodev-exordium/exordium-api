08eb9daad7b07116bea1ee27fffb4433a9f80d51
tvOdyssey <43388217+tvOdyssey@users.noreply.github.com>
2020-04-04 03:40:26 -0400

    Mongoose user model

---
+++ src/models/user.js
@@ -0,0 +1,57 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const uniqueValidator = require('mongoose-unique-validator');
+
+let userSchema = new Schema({
+
+    username: {
+        type: String
+    },
+    email: {
+        type: String,
+        unique: true
+    },
+    password: {
+        type: String
+    },
+    realname: {
+        type: String
+    },
+    picture: {
+        type: String
+    },
+    registration: {
+        country: {
+            type: String
+        },
+        ipAddress: {
+            type: String
+        },
+        registeredAt: {
+            type: Date
+        }
+    },
+    updated: {
+        ipAddress: {
+            type: String
+        },
+        updatedAt: {
+            type: Date
+        }
+    },
+    title: {
+        type: String
+    },
+    access: {
+        type: String
+    }
+    
+}, {
+    collection: 'users'
+})
+
+userSchema.plugin(uniqueValidator, { 
+    message: 'Email already in use.' 
+});
+
+module.exports = mongoose.model('User', userSchema)
