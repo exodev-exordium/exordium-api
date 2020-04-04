639c0b770beaaef17f1b6f5b4979c5ee322d8757
tvOdyssey <43388217+tvOdyssey@users.noreply.github.com>
2020-04-04 13:29:19 -0400

    Configure the mongodb file a little better

--- src/database/db.js
+++ src/database/db.js
@@ -1,8 +1,18 @@
-// localdb
-//var databaseConnect = `mongodb://${process.env.DB_HOSTNAME}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`
+let databaseConnect;
 
-// remotedb
-var databaseConnect = `mongodb://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOSTNAME}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`
+var db_username = process.env.DB_USERNAME || null,
+    db_password = process.env.DB_PASSWORD || null,
+    db_hostname = process.env.DB_HOSTNAME || 'localhost',
+    db_port = process.env.DB_PORT || 80,
+    db_database = process.env.DB_DATABASE || 'exordium';
+
+if (db_username === null & db_password === null) {
+    databaseConnect = `mongodb://${db_hostname}:${db_port}/${process.env.DB_DATABASE}`
+} else {
+    databaseConnect = `mongodb://${db_username}:${db_password}@${db_hostname}:${db_port}/${db_database}`
+}
+
+console.log(`Mongoose Connect: \t${databaseConnect}`);
 
 module.exports = {
     db: databaseConnect
