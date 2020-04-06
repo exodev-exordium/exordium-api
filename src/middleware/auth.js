2eeda8bf552e11fb969f3528aa6f1409706faeee
tvOdyssey <43388217+tvOdyssey@users.noreply.github.com>
2020-04-06 04:59:35 -0400

    Dont just check the token, actually verify it is a legit client and user trying to access the api

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,15 +1,24 @@
 const dotenv = require('dotenv');
 const jwt = require("jsonwebtoken");
+const User = require('../models/user');
 
 // Grab the .env configuration
 dotenv.config();
 
-module.exports = (req, res, next) => {
+module.exports = async(req, res, next) => {
     try {
         const token = req.headers.authorization.split(" ")[1];
-        jwt.verify(token, process.env.APP_JWTKEY);
+        const data = jwt.verify(token, process.env.APP_JWTKEY);
+        
+        const user = await User.findOne({ _id: data.userId, email: data.email});
+        if (!user) {
+            throw new Error();
+        }
+
+        req.id = data.userId;
+        req.email = data.email;
         next();
     } catch (error) {
-        res.status(401).json({ message: "No token provided" });
+        res.status(401).json({ status: "error", message: "Not authorized to access this resource" });
     }
 };
