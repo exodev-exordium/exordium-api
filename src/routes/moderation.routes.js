19cad81e8303eb1c3f2e4f7e1a4fdbffd847d5e5
tvOdyssey <43388217+tvOdyssey@users.noreply.github.com>
2020-04-30 04:05:53 -0400

    Get a single user from the database, if the user has access to those moderation pages.

--- src/routes/moderation.routes.js
+++ src/routes/moderation.routes.js
@@ -47,6 +47,24 @@ router.route('/users').get(authorize, (req, res, next) => {
     }
 })
 
+// Get Single User
+router.route('/user/:id').get(authorize, (req, res, next) => {
+    if (checkAccessPage(req.access.pages, 'page', 'users')) {
+        userSchema.findById(req.params.id, (error, response) => {
+            if (error) {
+                return next(error)
+            } else {
+                res.status(200).json(response)
+            }
+        })
+    } else {
+        res.status(401).json({ 
+            status: "error", 
+            message: "Not authorized to access this resource" 
+        });
+    }
+})
+
 function checkAccessPage (array, key, value) {
     return array.some(object => object[key] === value);
 }
