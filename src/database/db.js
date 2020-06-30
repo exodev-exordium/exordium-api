c161f9edfc612cd9622ca740362fb6c657c333cd
Odyssey <43388217+tvOdyssey@users.noreply.github.com>
2020-06-30 01:45:15 -0400

    update; atlas connection possibilities?

--- src/database/db.js
+++ src/database/db.js
@@ -1,18 +1,23 @@
 let databaseConnect;
 
-var db_username = process.env.DB_USERNAME || null,
+var db_atlas =    process.env.DB_ATLAS || false,
+    db_username = process.env.DB_USERNAME || null,
     db_password = process.env.DB_PASSWORD || null,
     db_hostname = process.env.DB_HOSTNAME || 'localhost',
     db_port =     process.env.DB_PORT     || 27017,
     db_database = process.env.DB_DATABASE || 'exordium';
 
-if (db_username === null & db_password === null) {
-    databaseConnect = `mongodb://${db_hostname}:${db_port}/${process.env.DB_DATABASE}`
+if (db_atlas === true) {
+    databaseConnect = `mongodb+srv://${db_username}:${db_password}@${db_hostname}/${db_database}?retryWrites=true&w=majority`
 } else {
-    databaseConnect = `mongodb://${db_username}:${db_password}@${db_hostname}:${db_port}/${db_database}`
+    if (db_username === null & db_password === null) {
+        databaseConnect = `mongodb://${db_hostname}:${db_port}/${process.env.DB_DATABASE}`
+    } else {
+        databaseConnect = `mongodb://${db_username}:${db_password}@${db_hostname}:${db_port}/${db_database}`
+    }
 }
 
-console.log(`Mongoose Connect: \t${databaseConnect}`);
+console.log(`Mongoose Connection: \t${databaseConnect}`);
 
 module.exports = {
     db: databaseConnect
