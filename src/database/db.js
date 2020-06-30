87aadbe9ce11e5fdd082b6c42828e50806e4ddc2
Odyssey <43388217+tvOdyssey@users.noreply.github.com>
2020-06-30 02:09:22 -0400

    update; load atlas version if its in the .env

--- src/database/db.js
+++ src/database/db.js
@@ -1,20 +1,24 @@
 let databaseConnect;
 
-var db_atlas =    process.env.DB_ATLAS || false,
+var db_atlas =    process.env.DB_ATLAS || null,
     db_username = process.env.DB_USERNAME || null,
     db_password = process.env.DB_PASSWORD || null,
     db_hostname = process.env.DB_HOSTNAME || 'localhost',
     db_port =     process.env.DB_PORT     || 27017,
     db_database = process.env.DB_DATABASE || 'exordium';
 
-if (db_atlas === true) {
+if (db_atlas !== null) {
+
     databaseConnect = `mongodb+srv://${db_username}:${db_password}@${db_hostname}/${db_database}?retryWrites=true&w=majority`
+
 } else {
+
     if (db_username === null & db_password === null) {
         databaseConnect = `mongodb://${db_hostname}:${db_port}/${process.env.DB_DATABASE}`
     } else {
         databaseConnect = `mongodb://${db_username}:${db_password}@${db_hostname}:${db_port}/${db_database}`
     }
+    
 }
 
 console.log(`Mongoose Connection: \t${databaseConnect}`);
