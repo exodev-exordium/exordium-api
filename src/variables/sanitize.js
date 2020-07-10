80d94a349835a7a0d1a7c08fdbcf36dad063b32f
Odyssey <43388217+tvOdyssey@users.noreply.github.com>
2020-07-10 01:10:02 -0400

    add; function for sanitizing query selectors

---
+++ src/variables/sanitize.js
@@ -0,0 +1,15 @@
+// get rid of query selectors
+function sanitize(v) {
+    if (v instanceof Object) {
+        for (var key in v) {
+            if (/^\$/.test(key)) {
+                delete v[key];
+            } else {
+                sanitize(v[key]);
+            }
+        }
+    }
+    return v;
+};
+
+module.exports = sanitize;
