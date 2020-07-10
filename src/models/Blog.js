dd732db3b0f815ac6038c3c204110efaab091a79
Odyssey <43388217+tvOdyssey@users.noreply.github.com>
2020-07-10 02:01:20 -0400

    update; blog post schema

---
+++ src/models/Blog.js
@@ -0,0 +1,61 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+let blogSchema = new Schema({
+    header: {
+        title: {
+            type: String
+        },
+        subtitle: {
+            type: String
+        }
+    },
+    body: {
+        short: {
+            type: String
+        },
+        long: {
+            type: String
+        }
+    },
+    updated: {
+        person: {
+            id: {
+                type: String,
+            },
+            email: {
+                type: String,
+                lowercase: true
+            },
+        },
+        ipAddress: {
+            type: String
+        },
+        updatedAt: {
+            type: Date,
+            default: Date.now
+        }
+    },
+    created: {
+        person: {
+            id: {
+                type: String,
+            },
+            email: {
+                type: String,
+                lowercase: true
+            },
+        },
+        ipAddress: {
+            type: String
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now
+        }
+    }
+}, {
+    collection: 'blog'
+});
+
+module.exports = mongoose.model('Blog', blogSchema);
