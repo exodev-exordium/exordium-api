56478a80cf3c410a3de717903008be6849bd6e63
tvOdyssey <43388217+tvOdyssey@users.noreply.github.com>
2020-07-13 18:01:38 -0400

    update; clients auth organisation

--- src/routes/clients/clients.auth.js
+++ src/routes/clients/clients.auth.js
@@ -15,96 +15,93 @@ dotenv.config();
 const { recaptchaApi, recaptchaSecret } = require('../../variables/recaptcha');
 
 // Sign-up
-router.post("/register",
-    [
-        check('username')
-            .not()
-            .isEmpty()
-            .isLength({ min: 3 })
-            .withMessage('Name must be atleast 3 characters long'),
-        check('email', 'Email is required')
-            .not()
-            .isEmpty(),
-        check('country', 'Country is required')
-            .not()
-            .isEmpty(),
-        check('password', 'Password should be between 5 to 64 characters long')
-            .not()
-            .isEmpty()
-            .isLength({ min: 5, max: 64 }),
-        check('recaptcha')
-            .not()
-            .isEmpty()
-            .withMessage('Must provide valid recaptcha response')
-    ],
-    (req, res, next) => {
-        const errors = validationResult(req);
-        const ipAddress = req.headers['cf-connecting-ip'] || req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-        var URLAddress = req.protocol + '://' + req.get('host') + req.originalUrl;
+router.post("/register", [
+    check('username')
+        .not()
+        .isEmpty()
+        .isLength({ min: 3 })
+        .withMessage('Name must be atleast 3 characters long'),
+    check('email', 'Email is required')
+        .not()
+        .isEmpty(),
+    check('country', 'Country is required')
+        .not()
+        .isEmpty(),
+    check('password', 'Password should be between 5 to 64 characters long')
+        .not()
+        .isEmpty()
+        .isLength({ min: 5, max: 64 }),
+    check('recaptcha')
+        .not()
+        .isEmpty()
+        .withMessage('Must provide valid recaptcha response')
+], (req, res, next) => {
+    const errors = validationResult(req);
+    const ipAddress = req.headers['cf-connecting-ip'] || req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+    var URLAddress = req.protocol + '://' + req.get('host') + req.originalUrl;
 
-        console.log(`[${ipAddress}] | URL: ${URLAddress}`);
+    console.log(`[${ipAddress}] | URL: ${URLAddress}`);
 
-        if (!errors.isEmpty()) {
-            return res.status(422).jsonp(errors.array());
-        } else {
+    if (!errors.isEmpty()) {
+        return res.status(422).jsonp(errors.array());
+    } else {
 
-            var checkRecaptcha = `${recaptchaApi}secret=${recaptchaSecret}&response=${req.body.recaptcha}&remoteip=${ipAddress}`;
-            Request(checkRecaptcha, function(error, resp, body) {
-                body = JSON.parse(body);
+        var checkRecaptcha = `${recaptchaApi}secret=${recaptchaSecret}&response=${req.body.recaptcha}&remoteip=${ipAddress}`;
+        Request(checkRecaptcha, function(error, resp, body) {
+            body = JSON.parse(body);
 
-                console.log(`${checkRecaptcha}`);
-                console.log(body);
+            console.log(`${checkRecaptcha}`);
+            console.log(body);
 
-                if (body.success !== undefined && !body.success) {
-                    return res.status(422).jsonp({"status": "error", "message": "Captcha Validation failed"});
-                } else {
-                    bcrypt.hash(req.body.password, 10).then((hash) => {
-                        const user = new userSchema({
-                            username: req.body.username,
-                            email: req.body.email,
-                            password: hash,
-                            realname: req.body.realname || undefined,
-                            registration: {
-                                country: {
-                                    code: req.body.country.code,
-                                    name: req.body.country.name
-                                },
-                                ipAddress: ipAddress,
-                                registeredAt: new Date().now
+            if (body.success !== undefined && !body.success) {
+                return res.status(422).jsonp({"status": "error", "message": "Captcha Validation failed"});
+            } else {
+                bcrypt.hash(req.body.password, 10).then((hash) => {
+                    const user = new userSchema({
+                        username: req.body.username,
+                        email: req.body.email,
+                        password: hash,
+                        realname: req.body.realname || undefined,
+                        registration: {
+                            country: {
+                                code: req.body.country.code,
+                                name: req.body.country.name
                             },
-                            updated: {
-                                ipAddress: ipAddress,
-                                updatedAt: new Date().now
-                            },
-                            title: 'User',
-                            access: {
-                                roles: [
-                                    {
-                                        role: "user"
-                                    }
-                                ],
-                                pages: []
-                            }
+                            ipAddress: ipAddress,
+                            registeredAt: new Date().now
+                        },
+                        updated: {
+                            ipAddress: ipAddress,
+                            updatedAt: new Date().now
+                        },
+                        title: 'User',
+                        access: {
+                            roles: [
+                                {
+                                    role: "user"
+                                }
+                            ],
+                            pages: []
+                        }
+                    });
+                    user.save().then((response) => {
+                        res.status(201).json({
+                            status: "success",
+                            message: "User successfully created!",
+                            result: response
                         });
-                        user.save().then((response) => {
-                            res.status(201).json({
-                                status: "success",
-                                message: "User successfully created!",
-                                result: response
-                            });
-                        }).catch(error => {
-                            res.status(500).json({
-                                status: "error",
-                                error: error
-                            });
+                    }).catch(error => {
+                        res.status(500).json({
+                            status: "error",
+                            error: error
                         });
                     });
-                }
-            });
+                });
+            }
+        });
 
-        }
     }
-);
+});
 
 // Sign-in
 router.post("/signin", (req, res, next) => {
@@ -129,18 +126,21 @@ router.post("/signin", (req, res, next) => {
             getUser = user;
             return bcrypt.compare(req.body.password, user.password);
         }).then(response => {
+
             if (!response) {
                 return res.status(401).json({
                     status: "error",
                     message: "Authentication failed"
                 });
             }
+
             let jwtToken = jwt.sign({
                 email: getUser.email,
                 userId: getUser._id
             }, process.env.APP_JWTKEY, {
                 expiresIn: "2h"
             });
+
             res.status(200).json({
                 status: "success",
                 token: jwtToken,
@@ -148,6 +148,7 @@ router.post("/signin", (req, res, next) => {
                 _id: getUser._id,
                 username: getUser.username
             });
+            
         }).catch(err => {
             return res.status(401).json({
                 status: "error",
