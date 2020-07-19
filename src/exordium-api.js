b208fdab15e492e89061c241aef43a1ce941c652
Odyssey <43388217+tvOdyssey@users.noreply.github.com>
2020-07-19 00:55:33 -0400

    update; fix commenting, add logging

--- src/exordium-api.js
+++ src/exordium-api.js
@@ -7,15 +7,16 @@ Date: 2020-03-16
 // Modules
 const dotenv = require('dotenv');
 const path = require('path');
+const fs = require('fs')
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const morgan = require('morgan')
 
 // Grab the .env configuration
 dotenv.config();
 
-// Require Database Connection
-const dbConfig = require('./database/db');
+const dbConfig = require('./database/db'); // Require Database Connection
 
 // MongoDB conection
 mongoose.Promise = global.Promise;
@@ -31,77 +32,96 @@ mongoose.connect(dbConfig.db, {
   }
 )
 
-// Remove MongoDB warning error
-mongoose.set('useCreateIndex', true);
+mongoose.set('useCreateIndex', true); // Remove MongoDB warning error
 
 // Setup the server
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-// Uploads Router
-app.use('/public/uploads', express.static(path.join(__dirname, '../uploads')));
-// Contact Router
+
+/* =================================
+ *  MORGAN LOGGING
+ * ================================= */
+
+const accessLogStream = fs.createWriteStream(path.join(__dirname, '../logs/access.log'), { flags: 'a' })
+app.use(morgan(':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length]', { stream: accessLogStream }));
+
+
+/* =================================
+ *  PUBLIC ROUTING
+ * ================================= */
+
+app.use('/public/uploads/blog', express.static(path.join(__dirname, '../uploads/blog'))); // blog images
+app.use('/public/uploads/users', express.static(path.join(__dirname, '../uploads/users'))); // user avatar images
+
 const publicContact = require('./routes/public/public.contact');
-app.use('/public/contact', publicContact);
-// Blog Router
+app.use('/public/contact', publicContact); // contact router
+
 const publicBlog = require('./routes/public/public.blog');
-app.use('/public/blog', publicBlog);
-// Servers Router
+app.use('/public/blog', publicBlog); // blog Router
+
 //const publicServers = require('./routes/public/public.servers');
-//app.use('/public/servers', publicServers);
+//app.use('/public/servers', publicServers); // servers Router
 
 
-// Client Auth Routers
+/* =================================
+ *  CLIENT ROUTING
+ * ================================= */
+
 const clientAuth = require('./routes/clients/clients.auth');
-app.use('/auth', clientAuth);
-// Client Me Routers
+app.use('/auth', clientAuth); // Client Auth Routers
+
 const clientMe = require('./routes/clients/clients.me');
-app.use('/user/me', clientMe);
+app.use('/user/me', clientMe); // Client Me Routers
 
 
-// Management Blogs Routers
+/* =================================
+ *  MANAGEMENT ROUTING
+ * ================================= */
+
 //const managementWatchdog = require('./routes/management/management.watchdog');
-//app.use('/management/watchdog', managementWatchdog);
-// Management Contact Routers
+//app.use('/management/watchdog', managementWatchdog); // Management Blogs Routers
+
 const managementContact = require('./routes/management/management.contact');
-app.use('/management/contact', managementContact);
-// Management Users Routers
+app.use('/management/contact', managementContact); // Management Contact Routers
+
 const managementUsers = require('./routes/management/management.users');
-app.use('/management/users', managementUsers);
-// Management Blogs Routers
+app.use('/management/users', managementUsers); // Management Users Routers
+
 const managementBlogs = require('./routes/management/management.blog');
-app.use('/management/blogs', managementBlogs);
-// Management Dev Blog Routers
+app.use('/management/blogs', managementBlogs); // Management Blogs Routers
+
 //const managementDevBlog = require('./routes/management/management.devblog');
-//app.use('/management/devblog', managementDevBlog);
+//app.use('/management/devblog', managementDevBlog); // Management Dev Blog Routers
 
 
-// Create server on the PORT
-const port = process.env.APP_PORT || 3000;
-app.listen(port, () => {
-  console.log(`Exordium API. Running: \thttps://${process.env.APP_HOSTNAME}:${port}/`)
-});
+/* =================================
+ *  EXPRESS ERROR HANDLING
+ * ================================= */
 
-// Express error handling
 app.use((req, res, next) => {
   setImmediate(() => {
-      next(new Error(`Internal Server Error`));
+      next(new Error(`Request could not be completed. Internal server error.`));
   });
 });
 app.use(function (err, req, res, next) {
-  var IPAddress = req.headers['cf-connecting-ip'] || req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-  var URLAddress = req.protocol + '://' + req.get('host') + req.originalUrl;
-
   if (!err.statusCode) {
     err.statusCode = 500;
   }
 
-  console.error(`[${IPAddress}] | Status: ${err.statusCode}:\tURL: ${URLAddress}: ${err.message}`);
-
   res.status(err.statusCode).json({
     statusCode: err.statusCode,
-    message: err.message,
-    ipAddr: IPAddress
+    message: err.message
   });
+});
+
+
+/* =================================
+ *  CREATE SERVER
+ * ================================= */
+
+const port = process.env.APP_PORT || 3000;
+app.listen(port, () => {
+  console.log(`Exordium API. Running: \thttps://${process.env.APP_HOSTNAME}:${port}/`)
 });
